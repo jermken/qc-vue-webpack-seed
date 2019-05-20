2c9c852d665dc7bdbeb77be8396a81611e340e1e
qiucheng <qiucheng@yy.com>
2019-05-20 19:31:35 +0800

    打开编译日志

--- config/dev.config.js
+++ config/dev.config.js
@@ -21,13 +21,13 @@ module.exports = merge(baseConf, {
         open: false,
         inline: true,
         progress: true,
-        quiet: true
+        quiet: false
     },
     devtool: 'inline-source-map',
     plugins: [
-        new friendlyErrorsWebpackPlugin({
-            messages: ['compiled successfully.']
-        }),
+        // new friendlyErrorsWebpackPlugin({
+        //     messages: ['compiled successfully.']
+        // }),
         new webpack.HotModuleReplacementPlugin()
     ]
 })
